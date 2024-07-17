9b5e6ff2d0bf487217972d2437263384e59a46e0
chris james cade <chris.james.cade@gmail.com>
2024-07-17 11:50:39 +0100

    added assignment 2

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+ 
+    key.classList.add('playing');
+    audio.currentTime = 0; 
+    audio.play();
+  }
+ 
+  
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+  }
+ 
+  
+  function handleClick(e) {
+    const keyCode = this.getAttribute('data-key');
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    if (!audio) return;
+ 
+    this.classList.add('playing');
+    audio.currentTime = 0; 
+    audio.play();
+  }
+ 
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  keys.forEach(key => key.addEventListener('click', handleClick));
+ 
+  window.addEventListener('keydown', playSound);
